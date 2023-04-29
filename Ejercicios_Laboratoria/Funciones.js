73a77cfce4921314b26b8ba11371a73c98527e80
Andres Bolivar <andresbolivar07@hotmail.com>
2023-04-28 22:09:22 -0500

    Delete archive Funciones.js

--- Ejercicios_Laboratoria/Funciones.js
+++
@@ -1,9 +0,0 @@
-/*Edad en segundos: Crea una web que pida al usuario su edad en años, utilizando prompt(), 
-y devuelva su edad en segundos, utilizando document.write().*/
-
-
-
-
-
-
-document.write
