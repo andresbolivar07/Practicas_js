c87cf81e91a9415b7c37f2c5295503388b6ce380
Andres Bolivar <andresbolivar07@hotmail.com>
2023-05-02 18:37:02 -0500

    feat: resolved exercises #2
    age in seconds

--- Ejercicios_Laboratoria/functions.js
+++ Ejercicios_Laboratoria/functions.js
@@ -1,7 +1,17 @@
+//ejercicio 1
 const edadEnSegundos = (edad) => edad * 31557600;
 
+//ejercicio 2
 const tempFarenheit = (tempCelsius) => (tempCelsius*1.8) + 32;
 
+//ejercicio 3
+
+const netIncome = (saleValue, cost) => saleValue - cost;
+
+const tax = (netIncome, taxes) => netIncome*taxes;
+
+const totalIncome = (netIncome, tax) => netIncome - tax;
 
 
-export {edadEnSegundos, tempFarenheit};
+
+export {edadEnSegundos, tempFarenheit, netIncome, tax, totalIncome};
