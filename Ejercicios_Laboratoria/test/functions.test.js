a9f46ac9401e92ef8816f194b5b1ff66968afff4
Andres Bolivar <andresbolivar07@hotmail.com>
2023-06-27 18:44:25 -0500

    test: Unit test to isPrime function

--- Ejercicios_Laboratoria/test/functions.test.js
+++ Ejercicios_Laboratoria/test/functions.test.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-undef */
-import { edadEnSegundos, tempFarenheit, netIncome } from '../src/functions.js'
+import { ageInSeconds, tempFarenheit, netIncome, isPrime } from '../src/functions.js'
 
-describe('edadEnSegundos', () => {
+describe('ageInSeconds', () => {
     it('should be a function', () => {
-        expect(typeof edadEnSegundos).toBe('function');
+        expect(typeof ageInSeconds).toBe('function');
     });
 
     it('convert 25 years to seconds', () => {
-        expect(edadEnSegundos(25)).toBe(788940000);
+        expect(ageInSeconds(25)).toBe(788940000);
     });
 });
 
@@ -28,5 +28,23 @@ describe('netIncome', () => {
         expect(netIncome(1000, 400, 19)).toBe(486);
     });
 
+});
+
+describe('isPrime', () => {
+    it('should be a function', () => {
+        expect(typeof isPrime).toBe('function');
+    });
+    it('is prime number', () => {
+        expect(isPrime(11)).toBe(true);
+    });
+    it('is not prime number', () => {
+        expect(isPrime(9)).toBe(false);
+    });
+    it('is prime number', () => {
+        expect(isPrime(1)).toBe(true);
+    });
+    it('is not prime number', () => {
+        expect(isPrime(0)).toBe(false);
+    });
 });
 
