19081a276ddd6fc79075cfa17d61da713550822b
Andres Bolivar <andresbolivar07@hotmail.com>
2023-06-27 17:43:51 -0500

    fix: change in variable names

--- Ejercicios_Laboratoria/src/functions.js
+++ Ejercicios_Laboratoria/src/functions.js
@@ -1,7 +1,7 @@
-const edadEnSegundos = (edad) => edad * 31557600;
+const ageInSeconds = (age) => age * 31557600;
 
 const tempFarenheit = (tempCelsius) => (tempCelsius*1.8) + 32;
 
 const netIncome = (profits, cost, taxes) => (parseInt((profits - cost)*((100-taxes)/100)));
 
-export {edadEnSegundos, tempFarenheit, netIncome};
+export {ageInSeconds, tempFarenheit, netIncome};
